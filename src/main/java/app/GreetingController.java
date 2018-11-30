c5c456676c144baa51d21a4ed00bfd20d774372a
TheRebelYell <dark777sovereign@gmail.com>
2018-11-30 22:53:21 +0300

    initial project

---
+++ src/main/java/app/GreetingController.java
@@ -0,0 +1,25 @@
+package app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping
+    public String index(Map<String, Object> model) {
+        model.put("msg", "Hello, welcome!!!");
+        return "index";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+}
